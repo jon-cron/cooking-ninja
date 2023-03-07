4fd3945608ceee74447447044bddb53f4506ed56
jon-cron <jonathanacm95@gmail.com>
2023-03-07 16:13:20 -0700

    using params

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,7 +1,12 @@
 import "./Recipe.css";
-
 import React from "react";
-
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch.js";
 export default function Recipe() {
+  const param = useParams();
+  const { data, isPending, error } = useFetch(
+    `http://localhost:3000/recipes/?id=${param.id}`
+  );
+
   return <div>Recipe</div>;
 }
