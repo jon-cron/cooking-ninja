121e2707d07f87f927a08d61338350e3893bef5b
jon-cron <jonathanacm95@gmail.com>
2023-03-07 16:00:07 -0700

    Link is working with params today

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,19 +1,15 @@
 import "./Home.css";
 import { useFetch } from "../../hooks/useFetch.js";
 import React from "react";
-import { RecipeList } from "../../components/recipeList/RecipeList.js";
+import RecipeList from "../../components/recipeList/RecipeList.js";
 
 export default function Home() {
-  const {
-    data: recipes,
-    isPending,
-    error,
-  } = useFetch("http://localhost:3000/recipes");
+  const { data, isPending, error } = useFetch("http://localhost:3000/recipes");
   return (
     <div className="home">
       {error && <p>{error}</p>}
       {isPending && <p>Loading...</p>}
-      {recipes && <RecipeList />}
+      {data && <RecipeList recipes={data} />}
     </div>
   );
 }
