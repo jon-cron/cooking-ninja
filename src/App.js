338e99ab687b15f7c0ba1721df1d7150e26769b0
jon-cron <jonathanacm95@gmail.com>
2023-03-07 15:17:12 -0700

    router / routes made using latest react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home.js";
+import Create from "./pages/create/Create.js";
+import Search from "./pages/search/Search.js";
+import Recipe from "./pages/recipe/Recipe.js";
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/recipes" element={<Recipe />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App
+export default App;
