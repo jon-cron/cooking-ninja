b9d359c5bbbb48d755d71fe86967255e3b1590d1
jon-cron <jonathanacm95@gmail.com>
2023-03-07 15:42:57 -0700

    drawing to screen

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,19 @@
 import "./Home.css";
-
+import { useFetch } from "../../hooks/useFetch.js";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  const {
+    data: recipes,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3000/recipes");
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      {isPending && <p>Loading...</p>}
+      {recipes &&
+        recipes.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+    </div>
+  );
 }
