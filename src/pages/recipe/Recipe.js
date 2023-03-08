246736ae8805e7b934b16d5ecd324e3bd92fdac5
jon-cron <jonathanacm95@gmail.com>
2023-03-08 08:25:21 -0700

    refactoring recipe params

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -3,10 +3,14 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch.js";
 export default function Recipe() {
-  const param = useParams();
-  const { data, isPending, error } = useFetch(
-    `http://localhost:3000/recipes/?id=${param.id}`
-  );
-  console.log(data);
-  return <div>Recipe</div>;
+  // NOTE this works just fine but to limit dot notation when can drill into the param by simply using { id } as seen below
+  // const param = useParams();
+  const { id } = useParams();
+  const {
+    data: recipe,
+    isPending,
+    error,
+  } = useFetch(`http://localhost:3000/recipes/?id=${id}`);
+  console.log(recipe);
+  return <div>{id}</div>;
 }
