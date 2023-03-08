c9da2634c3a8615fd3b6f4ca2fe9b191cff8c60f
jon-cron <jonathanacm95@gmail.com>
2023-03-08 11:05:22 -0700

    searching started

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import React from "react";
+import SearchBar from "../searchBar/SearchBar.js";
 
 export default function Navbar() {
   return (
@@ -9,6 +10,7 @@ export default function Navbar() {
         <Link className="brand" to="/">
           <h1>Home</h1>
         </Link>
+        <SearchBar />
         <Link className="brand" to="/create">
           Create
         </Link>
---
+++ src/components/searchBar/SearchBar.js
@@ -0,0 +1,26 @@
+import "./SearchBar.css";
+
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchBar() {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`/search/?q=${search}`);
+    setSearch("");
+  };
+  return (
+    <div className="searchBar">
+      <form onSubmit={handleSearch}>
+        <input
+          placeholder="search recipes..."
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+      </form>
+    </div>
+  );
+}
