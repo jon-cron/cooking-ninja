b35ab532c0159c2cc77f3b5934eb9b8dd661fec2
jon-cron <jonathanacm95@gmail.com>
2023-03-08 09:15:08 -0700

    form is submitting and resetting inputs

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,7 +1,55 @@
 import "./Create.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+    resetInputs();
+  };
+  const resetInputs = () => {
+    setTitle("");
+    setCookingTime("");
+    setMethod("");
+  };
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+      {/* NOTE onSubmit automatically passes the event or "e" to the function */}
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe Title:</span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Recipe Method:</span>
+          <textarea
+            required
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+          />
+        </label>
+        <label>
+          <span>Recipe Cooking Time (minutes):</span>
+          <input
+            type="number"
+            required
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+          />
+        </label>
+        <button type="btn">Submit</button>
+      </form>
+    </div>
+  );
 }
