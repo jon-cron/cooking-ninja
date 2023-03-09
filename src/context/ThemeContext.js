b737f3e8aeaecc789d401b5f97c712778a0313ff
jon-cron <jonathanacm95@gmail.com>
2023-03-09 09:33:49 -0700

    making reducer

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,8 +1,12 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 export const ThemeContext = createContext();
 
+const themeReducer = () => {};
 export function ThemeProvider({ children }) {
+  useReducer(themeReducer, {
+    color: "blue",
+  });
   return (
     <ThemeContext.Provider value={{ color: "blue" }}>
       {children}
