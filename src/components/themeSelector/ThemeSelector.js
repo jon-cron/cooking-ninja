a9c28f3856b81c3de6172f8ed77914ae33c1e9cb
jon-cron <jonathanacm95@gmail.com>
2023-03-10 12:06:38 -0700

    buttons and button style

--- src/components/themeSelector/ThemeSelector.js
+++ src/components/themeSelector/ThemeSelector.js
@@ -3,5 +3,14 @@ import "./ThemeSelector.css";
 import { useTheme } from "../../hooks/useTheme.js";
 export default function ThemeSelector() {
   const { color, changeColor } = useTheme();
-  return <div>ThemeSelector</div>;
+  const themeColors = ["#58259c", "#249c6b", "#b70233"];
+  return (
+    <div className="theme-selector">
+      <div className="theme-buttons">
+        {themeColors.map((c) => (
+          <div key={c} onClick={changeColor(c)} style={{ background: c }} />
+        ))}
+      </div>
+    </div>
+  );
 }
