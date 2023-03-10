239ca50d39867b0e8d4ff3b17be86d1bd7d871fb
jon-cron <jonathanacm95@gmail.com>
2023-03-09 17:05:27 -0700

    push context

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -2,11 +2,19 @@ import { createContext, useReducer } from "react";
 
 export const ThemeContext = createContext();
 
-const themeReducer = () => {};
+const themeReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_COLOR":
+      return {};
+  }
+};
 export function ThemeProvider({ children }) {
-  useReducer(themeReducer, {
+  const [state, dispatch] = useReducer(themeReducer, {
     color: "blue",
   });
+  const changeColor = (color) => {
+    dispatch({ type: "CHANGE_COLOR", payload: color });
+  };
   return (
     <ThemeContext.Provider value={{ color: "blue" }}>
       {children}
