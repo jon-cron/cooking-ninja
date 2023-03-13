d1ede6ef68fb7c1e99d197575d99535e3af89918
jon-cron <jonathanacm95@gmail.com>
2023-03-13 11:21:05 -0600

    getting data by id

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,16 +1,32 @@
 import "./Recipe.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useFetch } from "../../hooks/useFetch.js";
+// import { useFetch } from "../../hooks/useFetch.js";
 import { useTheme } from "../../hooks/useTheme.js";
+import { projectFirestore } from "../../firebase/config.js";
 export default function Recipe() {
-  const { mode } = useTheme();
+  // FIXME the commented code was used when hosting a local json data base and is no longer needed
   // NOTE this works just fine but to limit dot notation when can drill into the param by simply using { id } as seen below
   // const param = useParams();
+  // const url = `http://localhost:3000/recipes/?id=${id}`;
+  // const { data: recipe, isPending, error } = useFetch(url);
+  const { mode } = useTheme();
   const { id } = useParams();
-  const url = `http://localhost:3000/recipes/?id=${id}`;
-  const { data: recipe, isPending, error } = useFetch(url);
-  console.log(recipe);
+  const [recipe, setRecipe] = useState();
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    setIsPending(true);
+    projectFirestore
+      .collection("recipes")
+      // NOTE .doc() is how we search through our db and with params
+      .doc(id)
+      .get()
+      .then((doc) => {
+        console.log(doc);
+      });
+  }, []);
+
   return (
     <div className={`recipe ${mode}`}>
       {error && <div>{error}</div>}
